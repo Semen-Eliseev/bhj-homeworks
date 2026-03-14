89eba43276ad529487800b2ea767c35337b617f4
Semen <eliseevsemnik@yandex.ru>
2026-03-14 10:40:46 +0300

    Внесены изменения по формированию структкуры и доработки добавления и удаления записей по клику

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,31 +1,21 @@
-document.getElementById("task__input").addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        const taskInput = document.getElementById("task__input");
-        const taskText = taskInput.value; 
-        if (taskText) { 
-            const taskDiv = document.createElement("div");
-            taskDiv.className = "task";
-         
-            const titleDiv = document.createElement("div");
-            titleDiv.className = "task__title";
-            titleDiv.textContent = taskText;
-          
-            const removeLink = document.createElement("a");
-            removeLink.href = "#";
-            removeLink.className = "task__remove";
-            removeLink.innerHTML = "&times;"; 
-
-            removeLink.addEventListener("click", function() {
-                taskDiv.remove(); 
-            });
-
-            taskDiv.appendChild(titleDiv);
-            taskDiv.appendChild(removeLink);
-            
-            document.getElementById("tasks__list").appendChild(taskDiv);
-
-            taskInput.value = "";
-            taskInput.focus();
-        };
-    };    
+document.getElementById("tasks__add").addEventListener('click', function(event) {
+    event.preventDefault();
+    const taskInput = document.getElementById("task__input");
+    const taskText = taskInput.value.trim(); 
+    const tasksList = document.getElementById("tasks__list");
+    if (taskText) { 
+        tasksList.insertAdjacentHTML('afterbegin', `
+            <div class="task">
+                <div class="task__title">
+                    ${taskText}
+                </div>
+                <a href="#" class="task__remove">&times;</a>
+            </div>
+        `);
+        taskInput.value = "";
+        taskInput.focus();
+    };
+    document.querySelector('.task__remove').addEventListener("click", function(e) {
+        e.target.parentElement.remove(); 
+    });     
 });

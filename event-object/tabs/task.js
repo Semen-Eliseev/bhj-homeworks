193f049ecbf93c0767d46eb708a31559a9d03d40
semen <semen@10.143.160.3>
2026-03-15 23:47:58 +0300

    Исправление клика по конкретному контейнеру id=tabs1

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,25 +1,27 @@
-const tabs = Array.from(document.querySelectorAll('.tab'));
-const contents = Array.from(document.querySelectorAll('.tab__content'));
-let switchTab = (activeTab)=> {
-    tabs.forEach(tab => {
-        tab.classList.remove('tab_active'); 
-    });
-    activeTab.classList.add('tab_active'); 
+document.getElementById("tabs1").addEventListener("click", function (e) {
+    e.preventDefault();
+    
+    const item = e.target.parentElement;
+    
+    if(e.target.classList.contains('tab')){
+        
+        list = document.querySelectorAll(".tab");
+        Array.from(list).forEach(element => {
+            element.classList.remove("tab_active");
+        });
+        
+        e.target.classList.add("tab_active");
 
-    const targetIndex = tabs.indexOf(activeTab);
+        const targetIndex = Array.from(list).indexOf(e.target);
+        const contents = Array.from(document.querySelector('.tab__contents').children);
+        contents.forEach((content, index) => {
+            content.classList.remove('tab__content_active');
+            if (index === targetIndex) {
+                content.classList.add('tab__content_active');
+            };
+        });
+    
+    };          
+     
 
-    contents.forEach((content, index) => {
-        content.classList.remove('tab__content_active');
-        if (index === targetIndex) {
-            content.classList.add('tab__content_active');
-        }
-    });
-};
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        console.log(tab);
-        switchTab(tab);
-                
-    });
-});    
+});
